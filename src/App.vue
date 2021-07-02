7cf7bf62323c2b89ac6bccaab8255c03ca746100
Sanskar Lamsal <lamsal9677@yahoo.com>
2021-07-01 18:29:51 -0700

    git router added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
   <div id="app">
-    <!-- <img alt="3" src="./Photos/1.jpg" /> -->
-    <onexone imgsource= "1.jpg"/>
-</div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import onexone from "./components/onexone.vue";
-
-export default {
-  name: "App",
-  components: {
-    //HelloWorld,
-    onexone
-  },
-};
-</script>
 
 <style>
 #app {
@@ -24,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
