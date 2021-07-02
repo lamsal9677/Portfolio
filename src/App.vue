a78698be70b7a91123c628ca8ea03268c5e9b69e
Sanskar Lamsal <lamsal9677@yahoo.com>
2021-07-01 18:25:10 -0700

    Setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <!-- <img alt="3" src="./Photos/1.jpg" /> -->
+    <onexone imgsource= "1.jpg"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import onexone from "./components/onexone.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    //HelloWorld,
+    onexone
+  },
+};
 </script>
 
 <style>
---
+++ src/components/onexone.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>
+      Jii
+      {{getSource}}
+    </h1>
+    <img v-bind:src="'./Photos/1.jpg '" /> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "onexone",
+  props: {
+    imgsource: String,
+  },
+  computed: {
+    getSource () {
+      //return "./Photos/1.jpg"
+      return '1.jpg'
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
