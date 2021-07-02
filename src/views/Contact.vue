a7f89ab061f1e1c1a008ebe38ebcce193c4e11bc
Sanskar Lamsal <lamsal9677@yahoo.com>
2021-07-01 19:04:03 -0700

    Added Contacts Nav Link and Cleared the File

---
+++ src/views/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Sanskar Lamsal Contact page</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
