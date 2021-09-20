7e67d33754feb9a5e232a02f9e9c4b0c4ce84ed4
Sanskar Lamsal <lamsal9677@yahoo.com>
2021-09-19 22:34:32 -0400

    Refreshed Minimal Design Start

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,50 @@
 <template>
   <div id="app">
-    <div id="socialmediaicon">
-      <SocialMediaIcon />
-      <SocialMediaIcon />
-      <SocialMediaIcon />
+    <div id="navbar">
+      <div id="name">
+        <div id="firstName">
+          Sanskar
+        </div>
+        <div id="lastName">
+          Lamsal
+        </div>
+      </div>
+      <div id="links">
+        <router-link to="/about" v-if="!isExpanded" id="link"
+          >ABOUT</router-link
+        >
+        <router-link to="/about" v-if="!isExpanded" id="link"
+          >SKILLS</router-link
+        >
+        <router-link to="/about" v-if="!isExpanded" id="link"
+          >WORKS</router-link
+        >
+      </div>
     </div>
-    <Sidebar id="sidebar" />
-    <router-view id="routerview" />
   </div>
 </template>
-
-<script>
-import SocialMediaIcon from "./components/SocialMediaIcon/SocialMediaIcon";
-import Sidebar from "./components/Sidebar/Sidebar.vue";
-export default {
-  name: "Home",
-  components: {
-    Sidebar,
-    SocialMediaIcon,
-  },
-};
-</script>
 <style>
 #app {
-  text-align: center;
-  display: flex;
-  background-color: rgb(229, 235, 230);
+  width: 100%;
+  font-size: 1.5rem;
 }
-#sidebar {
-  /* float: left; */
-  /* position: fixed; */
+#navbar {
   display: flex;
-  flex-direction: column;
-  height: 700px;
-  position: sticky; /*fixed*/
-  /* position: ; */
-  /* height: inherit; */
+  flex-direction: row;
 }
-#routerview {
+#name {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
 }
-
-#socialmediaicon {
-  display: flex;
-  height: 15px;
-  width: 15px;
-  position: fixed;
-  top: 20px;
-  right: 150px;
-  z-index: 1;
+#lastName {
+  color: green;
+}
+#links {
+  margin-left: auto;
+  margin-right: 0;
+}
+#link {
+  padding: 3%;
+  color: inherit;
 }
 </style>
-
-//<a href="#" class="float">
-    //   <div>
-    //     Resume
-    //   </div>
-    // // </a>
-
-// .float { // /* margin: auto; */ // font-size: 14px; // font-family: "Lucida
-Sans", "Lucida Sans Regular", "Lucida Grande", // "Lucida Sans Unicode", Geneva,
-Verdana, sans-serif; // position: fixed; // width: 60px; // height: 60px; //
-bottom: 40px; // right: 40px; // background-color: black; // color: #fff; //
-border-radius: 50px; // line-height: 60px; // /* text-align: center; */ //
-box-shadow: 2px 2px 3px #999; // }
